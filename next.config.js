2def96471b4af1228f880a9cabfa0018456bb8dc
soerjo <ryohastomo@gmail.com>
2023-03-20 22:29:49 +0700

    redirect hero-image page to dashboard

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => [
+    {
+      source: "/dashboard/hero-image",
+      destination: "/dashboard",
+      permanent: true,
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
