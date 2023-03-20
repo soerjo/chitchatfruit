33021f527728e79c0c961821b7a772813a3514d2
soerjo <ryohastomo@gmail.com>
2023-03-20 23:00:30 +0700

    update: change tmp folder location

--- src/lib/readFileJson.lib.ts
+++ src/lib/readFileJson.lib.ts
@@ -5,10 +5,7 @@ const constantDir = path.join(process.cwd());
 
 export const readFile = async <T>(filename: string): Promise<T> => {
   try {
-    const CONSTANT = readFileSync(
-      constantDir + "/src/tmp/" + filename,
-      "utf-8"
-    );
+    const CONSTANT = readFileSync(constantDir + "/tmp/" + filename, "utf-8");
     return JSON.parse(CONSTANT);
   } catch (error) {
     console.error(error);
@@ -18,7 +15,7 @@ export const readFile = async <T>(filename: string): Promise<T> => {
 
 export const writeFile = async (filename: string, data: any) => {
   try {
-    writeFileSync(constantDir + "/src/tmp/" + filename, JSON.stringify(data));
+    writeFileSync(constantDir + "/tmp/" + filename, JSON.stringify(data));
   } catch (error) {
     console.error(error);
   }
