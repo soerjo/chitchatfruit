a7bbbdcdb868c58097c1662476be33574b73b5d0
soerjo <ryohastomo@gmail.com>
2023-03-29 23:59:54 +0700

    update: finishing

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -10,21 +10,23 @@ export interface CardCompProps {
 
 const CardComp: React.FC<CardCompProps> = (props) => {
   return (
-    <div className="min-w-full md:min-w-[380px] bg-white rounded-[8px] overflow-hidden shadow-md">
-      <img
-        src={props.img}
-        alt={props.title}
-        className="w-full object-cover h-[150px]"
-      />
-      <div className="p-[20px]">
-        <h4 className="font-inter text-[20px] font-semibold capitalize">
-          {props.title}
-        </h4>
-        <p className="text-[14px] text-abu">{props.description}</p>
-        <p className="font-medium text-[12px]">Mulai dari</p>
-        <h4 className="font-bold text-[30px]">
-          Rp {numberWithCommas(props.price)}
-        </h4>
+    <div className="w-1/3  p-2">
+      <div className="bg-white rounded-[8px] overflow-hidden shadow-md h-full border-[1px]">
+        <img
+          src={props.img}
+          alt={props.title}
+          className="w-full object-cover h-[150px]"
+        />
+        <div className="p-[20px]">
+          <h4 className="font-inter text-[20px] font-semibold capitalize">
+            {props.title}
+          </h4>
+          <p className="text-[14px] text-abu">{props.description}</p>
+          <p className="font-medium text-[12px]">Mulai dari</p>
+          <h4 className="font-bold text-[30px]">
+            Rp {numberWithCommas(props.price)}
+          </h4>
+        </div>
       </div>
     </div>
   );
